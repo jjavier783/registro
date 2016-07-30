39d5b6cc9c528d9fbb39215fd63cbd4180cd3e25
jjavier783 <j_javier_784@hotmail.com>
2016-07-30 03:42:33 -0500

    Add files via upload

---
+++ agujas_de_buffon/agujas_de_buffon/src/logic/buffon.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logic;
+
+/**
+ *
+ * @author RA302
+ */
+public class buffon {
+    
+    public double solve(double needles, double n){
+        double value=(double)(2*needles)/n;
+        return  value;
+    }
+   
+}
---
+++ agujas_de_buffon/agujas_de_buffon/src/main/run.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import GUI.MainFrame;
+
+/**
+ *
+ * @author RA302
+ */
+public class run {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MainFrame mf=new MainFrame();
+        mf.setVisible(true);
+    }
+}
